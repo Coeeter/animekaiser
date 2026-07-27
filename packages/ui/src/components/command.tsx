37c0faead776a4b62696478cd1e9a4bb884dfa57
Coeeter <nasrullah01n@gmail.com>
2026-07-27 21:46:26 +0800

    fix(ui): make dialogs scroll within viewport

--- packages/ui/src/components/command.tsx
+++ packages/ui/src/components/command.tsx
@@ -53,10 +53,7 @@ function CommandDialog({
         <DialogDescription>{description}</DialogDescription>
       </DialogHeader>
       <DialogContent
-        className={cn(
-          "top-1/3 translate-y-0 overflow-hidden rounded-4xl! p-0",
-          className
-        )}
+        className={cn("overflow-hidden rounded-4xl! p-0", className)}
         showCloseButton={showCloseButton}
       >
         {children}
